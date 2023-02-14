fce829b6ae2180e8dc1d2e86166c35ef2c5b435f
Gavin Blakeman <gavin.blakeman@theblakemans.id.au>
2023-02-14 14:27:51 +0800

    Initial commit

---
+++ plugin_passwordVerifier_scrypt.pro
@@ -0,0 +1,66 @@
+#-----------------------------------------------------------------------------------------------------------------------------------
+#
+# PROJECT:            Engineering Workshop Tracker (engineeringShop)
+# FILE:								plugin_passwordVerifier_scrypt.pro
+# SUBSYSTEM:          Project File
+# LANGUAGE:						C++
+# TARGET OS:          LINUX
+# LIBRARY DEPENDANCE:	None.
+# NAMESPACE:          N/A
+# AUTHOR:							Gavin Blakeman.
+# LICENSE:            GPLv2
+#
+#                     Copyright 2022 Gavin Blakeman.
+#
+# OVERVIEW:						Project file for compiling the plugin
+#
+# HISTORY:            2022-07-05/GGB - File Created
+#
+#-----------------------------------------------------------------------------------------------------------------------------------
+
+TARGET = plugin_scrypt
+
+DESTDIR = ../engineeringShop/plugins/hashSchemes
+
+TEMPLATE = lib
+
+QT += core
+QT -= gui
+
+CONFIG += cmdline
+CONFIG += plugin no_plugin_name_prefix
+CONFIG -= app_bundle
+CONFIG += object_parallel_to_source
+
+QMAKE_CXXFLAGS += -std=c++20
+QMAKE_LFLAGS += -Wl,--version-script=$${PWD}/pluginPassword.version
+DEFINES += BOOST_THREAD_USE_LIB QT_GUI_LIB QT_CORE_LIB
+
+#OBJECTS_DIR = "objects"
+
+# The following define makes your compiler emit warnings if you use
+# any feature of Qt which as been marked deprecated (the exact warnings
+# depend on your compiler). Please consult the documentation of the
+# deprecated API in order to know how to port your code away from it.
+DEFINES += QT_DEPRECATED_WARNINGS
+
+# You can also make your code fail to compile if you use deprecated APIs.
+# In order to do so, uncomment the following line.
+# You can also select to disable deprecated APIs only up to a certain version of Qt.
+#DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x060000    # disables all the APIs deprecated before Qt 6.0.0
+
+INCLUDEPATH +=  \
+    "../engineeringShop" \
+    "../GCL" \
+    "../MCL" \
+    "../PCL" \
+    "../SCL" \
+    "/usr/local/lib" \
+
+SOURCES += \
+  source/plugin_hashScheme_scrypt.cpp
+
+HEADERS +=
+
+LIBS += -lcrypt
+
---
+++ source/plugin_hashScheme_scrypt.cpp
@@ -0,0 +1,141 @@
+﻿#include "include/pluginInterface/interfaceHashScheme.h"
+
+  // C++ library
+
+#include <memory>
+#include <random>
+
+  // Miscellaneous libraries
+
+#include <crypt.h>
+#include <GCL>
+
+  // engineeringShop header files
+
+
+namespace pluginHashScheme
+{
+  std::string const SCRYPT_SALTLENGTH       ("scrypt/SaltLength");
+  std::string const SCRYPT_CPUTIME          ("scrypt/CPUTime");
+
+  static GCL::CReaderSections const *configurationReader = nullptr;
+
+  /// @brief Initialise the plugin. Must be called before any password functions are called.
+  /// @param[in] cr: Pointer to the configuration file reader.
+  /// @throws None
+  /// /// @note If this is not called, then defaults will be used.
+  /// @version 2022-11-10/GGB - Function created.
+
+  void initialisePlugin(GCL::CReaderSections const *cr)
+  {
+    configurationReader = cr;
+  }
+
+  void __attribute__ ((destructor)) destroyPlugin()
+  {
+  }
+
+
+  /// @brief Returns a string containing the hashing method.
+  /// @returns Reference to the hashing method
+  /// @throws
+  /// @version 2022-11-10/GGB - Function created.
+
+  std::string const &hashMethod()
+  {
+    static std::string const method = "scrypt";
+
+    return method;
+  }
+
+  /// @brief Creates a salt and hash for a new password.
+  /// @param[in] salt: The salt created.
+  /// @param[in] hash: The hashed password.
+  /// @param[in] password: The new password to hash.
+  /// @version 2022-07-16/GGB - Function created.
+
+  void createHash(std::string &salt, std::string &hash, std::string const &password)
+  {
+    std::string chrs = "./0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+    salt.clear();
+    hash.clear();
+
+    std::optional<std::uint16_t> saltLength = configurationReader->tagValueUInt16(SCRYPT_SALTLENGTH);
+    std::optional<std::uint16_t> rounds = configurationReader->tagValueUInt16(SCRYPT_CPUTIME);
+
+    if (!rounds)
+    {
+      rounds = 9;
+    }
+
+    if (!saltLength)
+    {
+      saltLength = 32;
+    }
+
+    std::random_device rd;
+    std::default_random_engine e1(rd());
+    std::uniform_int_distribution<int> uniform_dist(0, chrs.length()-1);
+
+      // Generate the salt.
+
+    for (auto i = 0; i < saltLength; i++)
+    {
+      salt.push_back(chrs[uniform_dist(e1)]);
+    }
+    salt.push_back(0);
+
+      // Generate the hash
+
+    crypt_data data;
+    std::string prefix = "$7$";
+    char setting[CRYPT_GENSALT_OUTPUT_SIZE];
+
+    std::fill(reinterpret_cast<volatile char *>(&data), reinterpret_cast<volatile char *>(&data) + sizeof(data), 0);
+
+    crypt_gensalt_rn(prefix.c_str(), *rounds, salt.c_str(), *saltLength, setting, CRYPT_GENSALT_OUTPUT_SIZE);
+    crypt_r(password.c_str(), setting, &data);
+
+    std::string_view sv(data.output);
+
+    hash = sv;
+
+      // Zero out the memory.
+
+    std::fill(reinterpret_cast<volatile char *>(&data), reinterpret_cast<volatile char *>(&data) + sizeof(data), 0);
+  }
+
+  /// @brief Authenticates the password against the hash.
+  /// @param[in] salt: The salt value.
+  /// @param[in] hash: The hashed password value.
+  /// @param[in] password: The password to verify.
+  /// @returns true if the password matches the hash.
+  /// @throws
+  /// @version 2022-11-10/GGB - Function created.
+
+  bool authenticate(std::string const &salt, std::string const &hash, std::string const &password)
+  {
+    bool returnValue = false;
+    crypt_data data;
+    char setting[CRYPT_GENSALT_OUTPUT_SIZE];
+
+    std::fill(reinterpret_cast<volatile char *>(&data), reinterpret_cast<volatile char *>(&data) + sizeof(data), 0);
+
+    crypt_r(password.c_str(), hash.c_str(), &data);
+
+    std::string_view sv(data.output);
+
+    returnValue = (sv == hash);
+
+    std::cout << "Return Value: " << returnValue << std::endl;
+
+      // Zero out the memory. It is on the stack so should get reused fairly quickly.
+
+    std::fill(reinterpret_cast<volatile char *>(&data), reinterpret_cast<volatile char *>(&data) + sizeof(data), 0);
+
+    return returnValue;
+  }
+
+
+} // namespace
